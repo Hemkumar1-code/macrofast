a1913fa2d23777681a513c9c81f37165ad734ebe
Hemkumar1-code <hemk3672@gmail.com>
2026-01-13 11:49:10 +0530

    Configure Vite for port 3000 and ngrok

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    allowedHosts: ['49ae919572b8.ngrok-free.app'],
+    host: true
+  }
 })
